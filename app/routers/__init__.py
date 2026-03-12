772e522e739a103cc484fc6b463739c1808f10e0
Shuv25 <shuvsutradhar@gmail.com>
2026-03-12 22:38:17 +0530

    fix: updated by adding the new retrieval router

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,5 +1,7 @@
 from app.routers.documents import document_router
 from app.routers.ingestion import ingestion_router
+from app.routers.retrieval import retrieval_router
 
 __all__=['document_router',
-         'ingestion_router']
+         'ingestion_router',
+         'retrieval_router']
