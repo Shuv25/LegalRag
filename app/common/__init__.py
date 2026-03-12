b98112932b2890479c5b7bc6f0d710fa77da88c7
Shuv25 <shuvsutradhar@gmail.com>
2026-03-12 22:28:30 +0530

    fix: updated the init by updating the new QueryInput pydantic model inside of it

--- app/common/__init__.py
+++ app/common/__init__.py
@@ -1,7 +1,8 @@
 from app.common.llm import router_llm
-from app.common.structured_models import QueryType
+from app.common.structured_models import QueryType, QueryModel
 
 __all__ =[
     'router_llm',
-    'QueryType'
+    'QueryType',
+    'QueryModel'
 ]
