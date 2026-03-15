c21a17f44ace53a0796c03ab1abeb102da98ce03
Shuv25 <shuvsutradhar@gmail.com>
2026-03-15 20:32:38 +0530

    feat: added the pydantic models for authentication

---
+++ app/common/user_model.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime, timezone
+
+class UserRegister(BaseModel):
+    fullname: str = Field(..., min_length=3, max_length=50)
+    email: EmailStr
+    password: str = Field(..., min_length=8, max_length=128)
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    token: str
+
+class DBUser(UserRegister):
+    role: str = "user"
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    is_active: bool = True
