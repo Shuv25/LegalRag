91e5c2065362970b5ce25093859e93a1915409f3
Shuv25 <shuvsutradhar@gmail.com>
2026-03-15 20:29:38 +0530

    feat: updated the endpoints with with admin permission

--- app/routers/documents.py
+++ app/routers/documents.py
@@ -4,13 +4,14 @@ Provides endpoints for fetching document metadata and removing documents
 from the registry.
 """
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
 #----------Import from other packages----------
 from logs.logger import get_logger
 from app.utils.registry import get_document, delete_document
+from app.core.security import require_admin
 
 logger = get_logger()
 limiter = Limiter(key_func=get_remote_address)
@@ -19,7 +20,7 @@ document_router = APIRouter()
 
 @document_router.get("/get_document", tags=["Documents"])
 @limiter.limit("5/minute")
-def find_document(request: Request,filename: str, matter: str) -> dict:
+def find_document(request: Request,filename: str, matter: str, current_user = Depends(require_admin)) -> dict:
     """
     Retrieves metadata of a specific document from the registry.
     :param filename: name of the PDF file
@@ -34,7 +35,8 @@ def find_document(request: Request,filename: str, matter: str) -> dict:
         raise
 
 @document_router.delete("/delete_document", tags=["Documents"])
-def remove_document(filename: str, matter: str) -> str:
+@limiter.limit("5/minute")
+def remove_document(request: Request,filename: str, matter: str, current_user = Depends(require_admin)) -> str:
     """
     Deletes a document's metadata from the registry.
     :param filename: name of the PDF file
