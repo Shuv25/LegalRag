bdbf3e1d90d370209c04ba818f18f99bb82949a3
Shuv25 <shuvsutradhar@gmail.com>
2026-03-18 01:30:26 +0530

    fix: added the load checkpointer

--- app/main.py
+++ app/main.py
@@ -24,6 +24,7 @@ from app.routers.documents import document_router
 from app.routers.retrieval import retrieval_router
 from app.routers.auth import auth_router
 from app.utils.mongo import connect as mongo_connect, disconnect as mongo_disconnect
+from app.utils.checkpointer import load_checkpointer
 from app.utils.pinecone_vdb import connect as pinecone_connect
 from app.utils.embeddings import load_model
 from app.utils.rerank import load_rerank_model
@@ -47,6 +48,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     try:
         logger.info("Starting up Legal RAG API...")
         mongo_connect()
+        app.state.checkpointer  = load_checkpointer()
         pinecone_connect(os.getenv("PINECONE_INDEX_NAME"))
         load_model()
         load_rerank_model()
